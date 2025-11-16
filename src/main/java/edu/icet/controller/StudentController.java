d2b8e1b8d8b1c56d847ce01d4d9d2c520eb5c352
Rashmi Wijemanna <rashmiwijemanna7@gmail.com>
2025-11-16 10:29:41 +0530

    add Studnet Controller class and service class

---
+++ src/main/java/edu/icet/controller/StudentController.java
@@ -0,0 +1,14 @@
+package edu.icet.controller;
+
+
+import edu.icet.model.dto.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("Student")
+
+public class StudentController {
+
+
+}
